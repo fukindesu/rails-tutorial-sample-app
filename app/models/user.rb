140592dc67f4c1e77f66fcddce5fc9a2d434aeac
fukindesu <63178773+fukindesu@users.noreply.github.com>
2022-07-19 22:38:23 +0900

    Implementing (finished up to 9.1.3)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { email.downcase! }
   
   validates :name, presence: true, length: { maximum: 50 }
@@ -8,6 +10,19 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
   def self.digest(string)
     cost = 
       if ActiveModel::SecurePassword.min_cost
@@ -16,5 +31,9 @@ class User < ApplicationRecord
         BCrypt::Engine.cost
       end
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
   end
 end
