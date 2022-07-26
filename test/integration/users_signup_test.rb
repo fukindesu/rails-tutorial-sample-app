c5a8b7b2e74e32c7e0c04684d3f3fe25773f5ebc
fukindesu <63178773+fukindesu@users.noreply.github.com>
2022-07-26 23:23:08 +0900

    Implementing (finished up to Practice 2 of 11.3.3)

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test 'invalid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
@@ -11,11 +15,26 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert-danger'
   end
 
-  test 'valid signup information' do
+  test 'valid signup information with account activation' do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: 'Example User', email: 'user@example.com', password: 'password', password_confirmation: 'password' } }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+
+    log_in_as(user) # before activation
+    assert_not is_logged_in?
+
+    get edit_account_activation_path('invalid token', email: user.email) # invalid token
+    assert_not is_logged_in?
+
+    get edit_account_activation_path(user.activation_token, email: 'wrong') # wrong email
+    assert_not is_logged_in?
+
+    get edit_account_activation_path(user.activation_token, email: user.email) # valid information
+    assert user.reload.activated?
     follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
